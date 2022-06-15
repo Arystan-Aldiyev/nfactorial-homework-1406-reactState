677f8d5112cc2e370886f301ac64ac50195f8abb
Arystan-Aldiyev <aldiyev.ars@gmail.com>
2022-06-16 00:34:39 +0600

    searchbox filter done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { v4 as myNewID } from "uuid";
+import { useState } from 'react';
+import { v4 as myNewID } from 'uuid';
 
 import "./App.css";
 
@@ -21,17 +21,18 @@ const buttons = [
 
 function App() {
   const [itemToDo, setItemToDo] = useState("");
+  const [itemToSearch, setItemToSearch] = useState("");
   const [items, setItems] = useState([
     {
-      key: 1,
+      key: myNewID(),
       label: "Have fun",
     },
     {
-      key: 2,
+      key: myNewID(),
       label: "Spread Empathy",
     },
     {
-      key: 3,
+      key: myNewID(),
       label: "Generate Value",
     },
   ]);
@@ -40,15 +41,32 @@ function App() {
 
   const handleToDoChange = (event) => {
     setItemToDo(event.target.value);
-  };
+  }
+
+  const handleSearch = (event) => {
+    setItemToSearch(event.target.value);
+  }
 
   const handleAddItem = () => {
-    const newItem = { key: myNewID(), label: itemToDo };
+    const newItem = { key: myNewID(), label: itemToDo }
 
-    setItems((prevElement) => [newItem, ...prevElement]);
+    // Push мутирует (as well as splice shift unshift) --- Плохо
+    // const newItems = items;
+    // newItems.push(newItem)
+    // setItems([...newItems])
+    if (itemToDo.trim() === "") {
 
+    } else {
+      setItems((prevItem) => [newItem, ...prevItem]);
+    }
     setItemToDo("");
-  };
+  }
+
+  const handleKeyPress = e => {
+    if (e.keyCode === 13) {
+      handleAddItem();
+    }
+  }
 
   const handleItemDone = ({ key }) => {
     setItems((prevItems) =>
@@ -60,47 +78,62 @@ function App() {
     );
   };
 
-  const handleFilterChange = ({ type }) => {
-    setFilterType(type);
+  const handleItemImportant = ({ key }) => {
+    setItems((prevItems) =>
+      prevItems.map((item) => {
+        ///function here
+        console.log("works")
+        if (item.key === key) {
+          return { ...item, important: !item.important };
+        } else return item;
+      })
+    );
   };
 
-  const moreToDo = items.filter((item) => !item.done).length;
+  const handleFilterChange = ({ type }) => {
+    setFilterType(type);
+  }
 
-  const doneToDo = items.length - moreToDo;
+  const moreToDo = items.filter((item) => !item.done).length;
+  const doneToDo = items.filter((item) => item.done).length;
 
   const filteredArray =
     filterType === "all"
       ? items
       : filterType === "done"
-      ? items.filter((item) => item.done)
-      : items.filter((item) => !item.done);
+        ? items.filter((item) => item.done)
+        : items.filter((item) => !item.done);
+
+  const filteredTwice = itemToSearch ? filteredArray.filter((item) => item.label.toLowerCase().includes(itemToSearch)) : filteredArray
+
 
   return (
     <div className="todo-app">
       {/* App-header */}
       <div className="app-header d-flex">
         <h1>Todo List</h1>
-        <h2>
-          {moreToDo} more to do, {doneToDo} done
-        </h2>
+        <h2>{moreToDo} more to do, {doneToDo} done</h2>
       </div>
 
       <div className="top-panel d-flex">
         {/* Search-panel */}
         <input
+          value={itemToSearch}
           type="text"
           className="form-control search-input"
           placeholder="type to search"
+          onChange={handleSearch}
         />
         {/* Item-status-filter */}
         <div className="btn-group">
+          {/* Button group */}
+
           {buttons.map((item) => (
             <button
               key={item.type}
               type="button"
-              className={`btn btn-info ${
-                filterType === item.type ? "" : "btn-outline-info"
-              }`}
+              className={`btn btn-info ${filterType === item.type ? "" : "btn-outline-info"
+                }`}
               onClick={() => handleFilterChange(item)}
             >
               {item.label}
@@ -108,22 +141,19 @@ function App() {
           ))}
         </div>
       </div>
-
       {/* List-group */}
       <ul className="list-group todo-list">
-        {filteredArray.length > 0 &&
-          filteredArray.map((item) => (
-            <li key={item.key} className="list-group-item">
-              <span className={`todo-list-item ${item.done ? "done" : ""}`}>
-                <span
-                  className="todo-list-item-label"
-                  onClick={() => handleItemDone(item)}
-                >
-                  {item.label}
-                </span>
+
+        {filteredTwice.length > 0 &&
+          filteredTwice.map((item) => (
+            <li key={item.key} className="list-group-item" >
+              <span className={`todo-list-item ${item.done ? "done" : ""} ${item.important ? "important" : ""}`}>
+                <span className="todo-list-item-label" onClick={() => handleItemDone(item)}>{item.label}</span>
 
                 <button
                   type="button"
+                  key={item.key}
+                  onClick={() => handleItemImportant(item)}
                   className="btn btn-outline-success btn-sm float-right"
                 >
                   <i className="fa fa-exclamation" />
@@ -147,10 +177,9 @@ function App() {
           className="form-control"
           placeholder="What needs to be done"
           onChange={handleToDoChange}
+          onKeyUp={handleKeyPress}
         />
-        <button className="btn btn-outline-secondary" onClick={handleAddItem}>
-          Add item
-        </button>
+        <button className="btn btn-outline-secondary" onClick={handleAddItem}>Add item</button>
       </div>
     </div>
   );
